4670e80e51287c6578a38f7ff207d03fb53416ec
linleck <linleckkyalsin26@gmail.com>
2023-07-12 20:04:51 +0630

    create regression expression test methods

--- src/test/java/com/reytech/regression_test/RegularExpressionTest.java
+++ src/test/java/com/reytech/regression_test/RegularExpressionTest.java
@@ -11,8 +11,8 @@ public class RegularExpressionTest
 {
     /**
      * Rigorous Test :-)
-     */
-    @Test
+     */      
+	@Test
     public void txtで終わるパターンに一致するかを検証するテスト()
     {
     	String pattern = "^.+\\.txt$";
@@ -23,30 +23,28 @@ public class RegularExpressionTest
     public void 四桁の半角数字の文字列パターンに一に一致するかを検証するテスト()
     {
     	String pattern = "^\\d{4}$";
-        assertTrue("1357".matches(pattern)); 
-        assertFalse("246810".matches(pattern)); 
+        assertTrue("1234".matches(pattern)); // 4桁の半角数字の文字列 "1234" はパターンに一致する
+        assertFalse("456".matches(pattern)); // 3桁の半角数字の文字列 "456" はパターンに一致しない
     }
     @Test
-    public void ひらがなで構成される文字列パターン一致するかを検証するテスト()
+    public void ひらがなで構成される文字列パターンに一に一致するかを検証するテスト()
     {
-    	String pattern = "^[ぁ-ん]+$";
-        assertTrue("ひらがな".matches(pattern)); 
-        assertFalse("カタカナ".matches(pattern)); 
+    	String pattern = "^[ぁ-んー]+$";
+        assertTrue("ひらがな".matches(pattern)); // ひらがなで構成される文字列 "ひらがな" はパターンに一致する
+        assertFalse("カタカナ".matches(pattern)); // カタカナを含む文字列 "カタカナ" はパターンに一致しない
     }
     @Test
-    public void デート形式の文字列パターンに一致するかを検証するテスト()
+    public void デート形式の文字列ターンに一致するかを検証するテスト()
     {
     	String pattern = "^\\d{4}-\\d{2}-\\d{2}$";
-        assertTrue("2023-07-11".matches(pattern)); 
-        assertFalse("2023/07/11".matches(pattern)); 
+        assertTrue("2023-07-09".matches(pattern)); // YYYY-MM-DD形式の文字列 "2023-07-09" はパターンに一致する
+        assertFalse("2023/07/09".matches(pattern)); // ハイフン以外の区切り文字を含む文字列 "2023/07/09" はパターンに一致しない
     }
     @Test
-    public void 制御文字を含まない文字列パターンに一致するかを検証するテスト()
+    public void 制御文字を含まない文字列ターンに一致するかを検証するテスト()
     {
     	String pattern = "^[^\\p{Cntrl}]+$";
-        assertTrue("こんばんは！".matches(pattern)); 
-        assertFalse("こんばんは！\n".matches(pattern)); 
+        assertTrue("Hello World".matches(pattern)); // 制御文字を含まない文字列 "Hello World" はパターンに一致する
+        assertFalse("こんにちは\n".matches(pattern)); // 改行文字を含む文字列 "こんにちは\n" はパターンに一致しない
     }
-    
-    
 }
