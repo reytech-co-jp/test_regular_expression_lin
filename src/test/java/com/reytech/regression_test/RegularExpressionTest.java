e4616b43982e6ead608165aacd99d6bf810687df
linleck <linleckkyalsin26@gmail.com>
2023-07-12 21:28:43 +0630

    fix:正義表現テストメソッドを修正する

--- src/test/java/com/reytech/regression_test/RegularExpressionTest.java
+++ src/test/java/com/reytech/regression_test/RegularExpressionTest.java
@@ -16,35 +16,45 @@ public class RegularExpressionTest
     public void txtで終わるパターンに一致するかを検証するテスト()
     {
     	String pattern = "^.+\\.txt$";
-        assertTrue("document.txt".matches(pattern)); 
-        assertFalse("text.doc".matches(pattern)); 
+        assertTrue("document.txt".matches(pattern));
+        assertTrue("myFile.txt".matches(pattern)); 
+        assertFalse("image.jpg".matches(pattern)); 
+        assertFalse("powerpoint.pptx".matches(pattern)); 
     }
     @Test
-    public void 四桁の半角数字の文字列パターンに一に一致するかを検証するテスト()
+    public void 四桁の半角数字の文字列パターンに一致するかを検証するテスト()
     {
     	String pattern = "^\\d{4}$";
-        assertTrue("1234".matches(pattern)); // 4桁の半角数字の文字列 "1234" はパターンに一致する
-        assertFalse("456".matches(pattern)); // 3桁の半角数字の文字列 "456" はパターンに一致しない
+        assertTrue("1234".matches(pattern)); 
+        assertTrue("5678".matches(pattern)); 
+        assertFalse("456".matches(pattern)); 
+        assertFalse("789".matches(pattern)); 
     }
     @Test
-    public void ひらがなで構成される文字列パターンに一に一致するかを検証するテスト()
+    public void ひらがなで構成される文字列パターンに一致するかを検証するテスト()
     {
     	String pattern = "^[ぁ-んー]+$";
-        assertTrue("ひらがな".matches(pattern)); // ひらがなで構成される文字列 "ひらがな" はパターンに一致する
-        assertFalse("カタカナ".matches(pattern)); // カタカナを含む文字列 "カタカナ" はパターンに一致しない
+        assertTrue("ひらがな".matches(pattern)); 
+        assertTrue("せいきひょうげん".matches(pattern)); 
+        assertFalse("カタカナ".matches(pattern)); 
+        assertFalse("Regular Expression".matches(pattern)); 
     }
     @Test
     public void デート形式の文字列ターンに一致するかを検証するテスト()
     {
-    	String pattern = "^\\d{4}-\\d{2}-\\d{2}$";
-        assertTrue("2023-07-09".matches(pattern)); // YYYY-MM-DD形式の文字列 "2023-07-09" はパターンに一致する
-        assertFalse("2023/07/09".matches(pattern)); // ハイフン以外の区切り文字を含む文字列 "2023/07/09" はパターンに一致しない
+    	String pattern = "^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$";
+        assertTrue("2001-02-26".matches(pattern)); 
+        assertTrue("2001-10-30".matches(pattern));
+        assertFalse("2001-26-02".matches(pattern)); 
+        assertFalse("2001/26/02".matches(pattern)); 
     }
     @Test
     public void 制御文字を含まない文字列ターンに一致するかを検証するテスト()
     {
     	String pattern = "^[^\\p{Cntrl}]+$";
-        assertTrue("Hello World".matches(pattern)); // 制御文字を含まない文字列 "Hello World" はパターンに一致する
-        assertFalse("こんにちは\n".matches(pattern)); // 改行文字を含む文字列 "こんにちは\n" はパターンに一致しない
+        assertTrue("Hello Lin Leck!".matches(pattern));
+        assertTrue("リンさん、こんにちは!".matches(pattern));
+        assertFalse("Hello Lin\tLeck!".matches(pattern)); 
+        assertFalse("リンさん、こんにちは!\n".matches(pattern)); 
     }
 }
