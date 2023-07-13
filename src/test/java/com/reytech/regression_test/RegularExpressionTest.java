a3a4b96014fb45dbec30bc9b21474547e67d9bf3
linleck <linleckkyalsin26@gmail.com>
2023-07-13 15:32:29 +0630

    改行を消す

--- src/test/java/com/reytech/regression_test/RegularExpressionTest.java
+++ src/test/java/com/reytech/regression_test/RegularExpressionTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 public class RegularExpressionTest {
 
 	@Test
-
 	public void txtで終わるパターンに一致するかを検証するテスト() {
 		String pattern = "^.+\\.txt$";
 		assertTrue("document.txt".matches(pattern));
@@ -18,7 +17,6 @@ public class RegularExpressionTest {
 	}
 
 	@Test
-
 	public void 四桁の半角数字の文字列パターンに一致するかを検証するテスト() {
 		String pattern = "^\\d{4}$";
 		assertTrue("1234".matches(pattern));
@@ -28,7 +26,6 @@ public class RegularExpressionTest {
 	}
 
 	@Test
-
 	public void ひらがなで構成される文字列パターンに一致するかを検証するテスト() {
 		String pattern = "^[ぁ-んー]+$";
 		assertTrue("ひらがな".matches(pattern));
@@ -38,7 +35,6 @@ public class RegularExpressionTest {
 	}
 
 	@Test
-
 	public void デート形式の文字列ターンに一致するかを検証するテスト() {
 		String pattern = "^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$";
 		assertTrue("2001-02-26".matches(pattern));
@@ -48,7 +44,6 @@ public class RegularExpressionTest {
 	}
 
 	@Test
-
 	public void 制御文字を含まない文字列ターンに一致するかを検証するテスト() {
 		String pattern = "^[^\\p{Cntrl}]+$";
 		assertTrue("Hello Lin Leck!".matches(pattern));
